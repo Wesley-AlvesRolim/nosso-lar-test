4979755028c86fce360136b32d6f2588f206cb00
Wesley Alves Rolim <walvesrolim@gmail.com>
2023-10-01 16:30:41 -0300

    feat(filter-empty-values-from-object): ensure it removes empty values like null, undefined and empty array

--- question-1/src/adapter/filter-empty-values-from-json/filter-empty-values-from-object.ts
+++ question-1/src/adapter/filter-empty-values-from-json/filter-empty-values-from-object.ts
@@ -1,3 +1,22 @@
+import { isEmptyValue, isJsObject } from "@/validation";
+
+const filterEmptyValues = (value: any) => {
+  for (const key in value) {
+    const currentValue = value[key];
+
+    if (isEmptyValue(currentValue)) {
+      delete value[key];
+    }
+
+    if (isJsObject(currentValue)) {
+      filterEmptyValues(currentValue);
+    }
+  }
+
+  return value;
+};
+
 export const filterEmptyValuesFromObject = (obj: Object) => {
-  return obj;
+  const copyObj = { ...obj };
+  return filterEmptyValues(copyObj);
 };

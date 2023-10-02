80da9fffe44454992368ee8431ada410ff5b50fd
Wesley Alves Rolim <walvesrolim@gmail.com>
2023-10-02 09:28:08 -0300

    fix: removes empty arrays in expected output

--- question-1/src/adapter/filter-empty-values-from-array/filter-empty-values-from-array.spec.ts
+++ question-1/src/adapter/filter-empty-values-from-array/filter-empty-values-from-array.spec.ts
@@ -10,17 +10,17 @@ describe("filterEmptyValuesFromArray", () => {
       ["array", [], null, 0, "string", 93],
     ];
 
-    const expectedOutput = ["value", [1], 842, ["array", [], 0, "string", 93]];
+    const expectedOutput = ["value", [1], 842, ["array", 0, "string", 93]];
 
     const output = filterEmptyValuesFromArray(input);
-    expect(output).toEqual(expect.objectContaining(expectedOutput));
+    expect(output).toEqual(expect.arrayContaining(expectedOutput));
   });
 
   it("should not filter a valid array", () => {
-    const input = ["value", [1], 842, ["array", [], 0, "string", 93]];
-    const expectedOutput = ["value", [1], 842, ["array", [], 0, "string", 93]];
+    const input = ["value", [1], 842, ["array", 0, "string", 93]];
+    const expectedOutput = ["value", [1], 842, ["array", 0, "string", 93]];
 
     const output = filterEmptyValuesFromArray(input);
-    expect(output).toEqual(expect.objectContaining(expectedOutput));
+    expect(output).toEqual(expect.arrayContaining(expectedOutput));
   });
 });

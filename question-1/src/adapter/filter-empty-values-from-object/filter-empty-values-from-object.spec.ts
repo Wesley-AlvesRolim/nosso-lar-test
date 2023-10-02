eb77dc845368862f24ddaf39f3dca299e567bb56
Wesley Alves Rolim <walvesrolim@gmail.com>
2023-10-02 09:26:15 -0300

    test(filter-empty-values-from-object): ensure it also filter nested arrays with empty values

--- question-1/src/adapter/filter-empty-values-from-object/filter-empty-values-from-object.spec.ts
+++ question-1/src/adapter/filter-empty-values-from-object/filter-empty-values-from-object.spec.ts
@@ -21,4 +21,26 @@ describe("filterEmptyValuesFromObject", () => {
     const output = filterEmptyValuesFromObject(input);
     expect(output).toEqual(expect.objectContaining(expectedOutput));
   });
+
+  it("should filter empty values from arrays inside of object", () => {
+    const input = {
+      key1: "value1",
+      key2: null,
+      key3: {
+        key4: ["array1", null],
+        key5: [[1, null], []],
+      },
+    };
+
+    const expectedOutput = {
+      key1: "value1",
+      key3: {
+        key4: ["array1"],
+        key5: [[1]],
+      },
+    };
+
+    const output = filterEmptyValuesFromObject(input);
+    expect(output).toEqual(expect.objectContaining(expectedOutput));
+  });
 });

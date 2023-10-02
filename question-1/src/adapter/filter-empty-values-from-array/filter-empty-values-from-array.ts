1bf8a9c646f4f902baa8c97a4801bcefde8c720d
Wesley Alves Rolim <walvesrolim@gmail.com>
2023-10-02 09:05:11 -0300

    feat(filter-empty-values-from-array): implementation of the filter with nested array

--- question-1/src/adapter/filter-empty-values-from-array/filter-empty-values-from-array.ts
+++ question-1/src/adapter/filter-empty-values-from-array/filter-empty-values-from-array.ts
@@ -1,4 +1,17 @@
+const filterEmptyValues = <T>(arr: T[]): void => {
+  for (let i = 0; i < arr.length; i++) {
+    if (Array.isArray(arr[i])) {
+      filterEmptyValues(arr[i] as unknown as unknown[]);
+    }
+
+    if (arr[i] === null || arr[i] === undefined) {
+      arr.splice(i, 1);
+    }
+  }
+};
+
 export const filterEmptyValuesFromArray = <T>(arr: T[]): T[] => {
   const copyArr = [...arr];
+  filterEmptyValues(copyArr);
   return copyArr;
 };
---
+++ question-1/src/adapter/index.ts
@@ -0,0 +1,2 @@
+export * from "./filter-empty-values-from-array/filter-empty-values-from-array";
+export * from "./filter-empty-values-from-object/filter-empty-values-from-object";
